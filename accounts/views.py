9058c93cd2b0e138c36e77d6a50bec1de4ec53f2
Abdul Moiz <progrmoiz@gmail.com>
2020-03-10 21:43:11 +0500

    added password change

--- accounts/views.py
+++ accounts/views.py
@@ -6,12 +6,17 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.detail import SingleObjectMixin
 from django.urls import reverse
 from django.views import View
-from .forms import StudentForm, LoginForm
+from .forms import StudentForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Student
 from .utils import get_current_student
 from django.views.generic.edit import FormView
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext as _
 
 class HomePageView(View):
     template_name = 'home.html'
@@ -20,6 +25,37 @@ class HomePageView(View):
         student = Student.objects.get(user__username=request.user.username)
 
         return render(request, self.template_name, { 'student': student })
+
+class ChangePasswordView(View):
+    template_name = 'change_password.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ChangePasswordView, self).dispatch(*args, **kwargs)
+
+    def get(self, request):
+        student = get_current_student(request)
+
+        form = PasswordChangeForm(request.user)
+
+        context = {
+            'is_account_page': 'active',
+            'form': form,
+            'student': student,
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, _('Your password was successfully updated!'))
+            return redirect('change_password')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+            return redirect('change_password')
 
 class AccountView(TemplateView):
     template_name = 'profile.html'
@@ -81,13 +117,16 @@ class LoginView(View):
         if user:
             if user.is_active:
                 login(request,user)
+                messages.success(request, 'Successfully logged in!')
                 return HttpResponseRedirect(reverse('index'))
             else:
-                return HttpResponse("Your account was inactive.")
+                messages.warning(request, 'Your account was inactive.')
+                return redirect('login')
         else:
             print("Someone tried to login and failed.")
             print("They used username: {} and password: {}".format(username,password))
-            return HttpResponse("Invalid login details given")
+            messages.error(request, 'Invalid login details given')
+            return redirect('login')
 
     def get(self, request):
         if request.user.is_authenticated:
