a98a258c00c76d2c95e71ae501dfbf56e060ada5
Abdul Moiz <progrmoiz@gmail.com>
2020-03-12 22:05:32 +0500

    updated with announcement

--- announcement/views.py
+++ announcement/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Announcement, AnnouncementFilter
 from university.models import Department
 from course.models import CourseEnrollment, CourseOffering
@@ -38,3 +39,20 @@ class AnnouncementListView(ListView):
         a_c = a_d.filter(Q(announcement_filters__course__in=student_courses))
 
         return a_g | a_s | a_c | a_d
+
+class AnnouncementDetailView(DetailView):
+    # template_name = 'announcement/announcement_detail.html'
+    model = Announcement
+    context_object_name = 'announcement'
+
+    def get_context_data(self, **kwargs):
+        context = super(AnnouncementDetailView,self).get_context_data(**kwargs)
+        context['is_announcement_page'] = 'active'
+        context['student'] = get_current_student(self.request)
+        return context
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Announcement.objects.filter(active=True)
+        else:
+            return Announcement.objects.none()
