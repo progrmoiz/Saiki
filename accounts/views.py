9d77051c0edf384793382d3bb0767d1dc3d5b22e
Abdul Moiz <progrmoiz@gmail.com>
2020-11-27 03:45:53 +0500

    updated with accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import SingleObjectMixin
 from django.urls import reverse
 from django.views import View
-from .forms import StudentForm
+from .forms import StudentForm, TeacherForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Student
-from .utils import get_current_student, is_student, is_teacher
+from .utils import get_current_student, get_current_teacher, is_student, is_teacher
 from django.views.generic.edit import FormView
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
@@ -38,12 +39,9 @@ class HomePageView(View):
         
         return HttpResponseForbidden()
 
-class ChangePasswordView(View):
+class ChangePasswordView(LoginRequiredMixin, View):
+    redirect_field_name = 'accounts:login'
     template_name = 'accounts/change_password.html'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(ChangePasswordView, self).dispatch(*args, **kwargs)
 
     def get(self, request):
         # TODO: add student login
@@ -77,7 +75,8 @@ class ChangePasswordView(View):
             messages.error(request, _('Please correct the error below.'))
             return redirect('accounts:change_pwd')
 
-class AccountView(TemplateView):
+class AccountView(LoginRequiredMixin, TemplateView):
+    redirect_field_name = 'accounts:login'
     template_name = 'accounts/profile.html'
 
     def get_context_data(self, **kwargs):
@@ -89,10 +88,13 @@ class AccountView(TemplateView):
         context['is_account_page'] = 'active'
         context['meta'] = meta
 
-        context['cgpa'] = get_current_student(self.request).get_cgpa()
+        if is_student(self.request):
+            context['cgpa'] = get_current_student(self.request).get_cgpa()
+
         return context
 
-class ForgetPasswordView(TemplateView):
+class ForgetPasswordView(LoginRequiredMixin, TemplateView):
+    redirect_field_name = 'accounts:login'
     template_name = 'accounts/forget_password.html'
 
     def get_context_data(self, **kwargs):
@@ -105,21 +107,36 @@ class ForgetPasswordView(TemplateView):
         context['meta'] = meta
         return context
 
-class AccountEditView(FormView):
+class AccountEditView(LoginRequiredMixin, FormView):
+    redirect_field_name = 'accounts:login'
     template_name = 'accounts/edit.html'
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(AccountEditView, self).dispatch(*args, **kwargs)
-
     def get(self, request):
-        student = get_current_student(request)
 
-        if student:
-            form = StudentForm(initial={'display_name': student.display_name, 'phone_number': student.phone_number, 'telephone': student.telephone, 'address_1': student.address_1, 'address_2': student.address_2})
+        if request.user.is_teacher:
+            teacher = get_current_teacher(request)
+            
+            form = TeacherForm(initial={
+                'first_name': teacher.user.first_name, 
+                'last_name': teacher.user.last_name, 
+                'display_name': teacher.display_name, 
+                'email': teacher.user.email, 
+            })
+        elif request.user.is_student:
+            student = get_current_student(request)
+
+            form = StudentForm(initial={
+                'first_name': student.user.first_name, 
+                'last_name': student.user.last_name, 
+                'display_name': student.display_name, 
+                'email': student.user.email, 
+                'phone_number': student.phone_number, 
+                'telephone': student.telephone, 
+                'address_1': student.address_1, 
+                'address_2': student.address_2
+            })
         else:
-            form = StudentForm()
-        
+            return HttpResponseForbidden()
         meta = Meta(
             title=get_site_title('Edit profile')
         )
@@ -127,28 +144,44 @@ class AccountEditView(FormView):
         context = {
             'is_account_page': 'active',
             'form': form,
-            'is_student': True,
-            'student': student,
             'meta': meta
         }
 
         return render(request, self.template_name, context)
 
     def post(self, request):
-        form = StudentForm(request.POST)
-        if form.is_valid:
+        if request.user.is_student:
+            form = StudentForm(request.POST)
+        elif request.user.is_teacher:
+            form = TeacherForm(request.POST)
+        else:
+            return HttpResponseForbidden()
+        
+        if form.is_valid():
             data = request.POST.copy()
-            print(data)
 
-            student = Student.objects.get(user__username=request.user.username)
-            student.display_name = data.get('display_name')
-            student.phone_number = data.get('phone_number')
-            student.telephone = data.get('telephone')
-            student.address_1 = data.get('address_1')
-            student.address_2 = data.get('address_2')
-            student.save()
+            if request.user.is_student:
+                student = Student.objects.get(user__username=request.user.username)
+                student.user.first_name = form.cleaned_data['first_name']
+                student.user.last_name = form.cleaned_data['last_name']
+                student.display_name = form.cleaned_data['display_name']
+                student.phone_number = form.cleaned_data['phone_number']
+                student.user.email = form.cleaned_data['email']
+                student.telephone = form.cleaned_data['telephone']
+                student.address_1 = form.cleaned_data['address_1']
+                student.address_2 = form.cleaned_data['address_2']
+                student.save()
+                student.user.save()
+            elif request.user.is_teacher:
+                teacher = get_current_teacher(request)
+                teacher.user.first_name = form.cleaned_data['first_name']
+                teacher.user.last_name = form.cleaned_data['last_name']
+                teacher.display_name = form.cleaned_data['display_name']
+                teacher.user.email = form.cleaned_data['email']
+                teacher.save()
+                teacher.user.save()
 
-            return HttpResponse('submitted')
+            return redirect('accounts:edit')
 
 class LoginView(View):
     template_name = 'accounts/login.html'
@@ -184,7 +217,6 @@ class LoginView(View):
             context = {
                 'meta': meta
             }
-            
             return render(request, self.template_name, context)
 
 class LogoutView(View):
